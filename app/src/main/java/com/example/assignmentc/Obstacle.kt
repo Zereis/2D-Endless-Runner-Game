66fa014189609d525620914566af2725d757f5ec
Kevin Bjørn Roulund Wilken <kwilken1608@hotmail.dk>
2023-05-23 12:42:15 +0200

    Obstacle Class Setup

--- app/src/main/java/com/example/assignmentc/Obstacle.kt
+++ app/src/main/java/com/example/assignmentc/Obstacle.kt
@@ -1,10 +1,33 @@
 package com.example.assignmentc
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.graphics.PointF
+import android.graphics.Rect
+import android.graphics.RectF
 
-class Obstacle {
+class Obstacle(private val context: Context) {
+
+    var paint: Paint? = null
+    var posX: Float = 0.0f
+    var posY: Float = 0.0f
+
+    /*
     var position: PointF =
         PointF(0f, 0f)
     var speed: PointF = PointF(0f, 0f)
+    */
 
+    init {
+        paint = Paint()
+        paint!!.isFilterBitmap = true
+        paint!!.isAntiAlias = true
+        paint!!.color = Color.BLACK
+        posX = 100.0f
+        posY = 100.0f
+    }
 }
