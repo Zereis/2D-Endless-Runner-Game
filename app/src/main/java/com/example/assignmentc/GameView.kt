8ecfba700d7e2c3760a9ac5792b3abb51ff2db49
David Hoang <62556785+Zereis@users.noreply.github.com>
2023-05-23 13:27:11 +0200

    Fixed player movement

--- app/src/main/java/com/example/assignmentc/GameView.kt
+++ app/src/main/java/com/example/assignmentc/GameView.kt
@@ -15,14 +15,22 @@ class GameView(context: Context) : View(context) {
     var startPositionX: Float = 550f
     var startPositionY: Float = 1800f
 
-    private val SWIPE_THRESHOLD = 100
-    var test: Boolean = false
+    var initialTouchX = 0f // Initial touch position X-coordinate
+    var isLaneSwitching = false // Flag to track lane switch state
 
+    private val SWIPE_THRESHOLD = 100
+
+
+    var leftLane: Int? = 1
+    var middleLane: Int? = 2
+    var rightLane: Int? = 3
+
+    var playerCurrentLane = 2
 
     // Lane X coordinates for positioning.
-    var leftLaneX: Int? = 1
-    var middleLaneX: Int? = 2
-    var rightLaneX: Int? = 3
+    //var leftLaneX: Int? = null
+    //var middleLaneX: Int? = null
+    //var rightLaneX: Int? = null
 
     // Y spawn position for objects
     var spawnY: Int? = null
@@ -33,8 +41,8 @@ class GameView(context: Context) : View(context) {
     init {
         player = Player(context)
 
-        setUpLanes()
-        startPositionX = rightLaneX!!.toFloat()
+        //setUpLanes()
+        //startPositionX = rightLaneX!!.toFloat()
 
 
 
@@ -46,36 +54,68 @@ class GameView(context: Context) : View(context) {
         canvas?.drawColor(Color.RED)
 
 
-        if(test)
+        if(playerCurrentLane == leftLane)
         {
             canvas?.drawCircle(200f,startPositionY, 50f, player.paint!!)
         }
-        else
+        else if(playerCurrentLane == middleLane)
         {
-            canvas?.drawCircle(startPositionX,startPositionY, 50f, player.paint!!)
+            canvas?.drawCircle(550f,startPositionY, 50f, player.paint!!)
         }
+        else if(playerCurrentLane == rightLane)
+        {
+            canvas?.drawCircle(800f,startPositionY, 50f, player.paint!!)
+        }
+
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-                // Record the initial touch position
-                player.circleX = event.x
-                true
+                if (!isLaneSwitching) {
+                    initialTouchX = event.x
+                }
             }
             MotionEvent.ACTION_MOVE -> {
-                val deltaX = event.x - player.circleX
-                if (deltaX < SWIPE_THRESHOLD) {
-                    // Swiped left
-                    test = true
+                if (!isLaneSwitching) {
+                    val currentTouchX = event.x
+                    val distance = currentTouchX - initialTouchX
+
+                    // Determine swipe direction
+                    val swipeDirection = when {
+                        distance > 0 -> "Right"
+                        distance < 0 -> "Left"
+                        else -> ""
+                    }
+
+                    // Switch lanes based on swipe direction
+                    when (swipeDirection) {
+                        "Right" -> {
+                            if (playerCurrentLane < 3) {
+                                // Switch to the next lane (increase player's position)
+                                playerCurrentLane++
+                                // Update player's position gradually for smooth movement
+                                isLaneSwitching = true
+                            }
+                        }
+                        "Left" -> {
+                            if (playerCurrentLane > 1) {
+                                // Switch to the previous lane (decrease player's position)
+                                playerCurrentLane--
+                                // Update player's position gradually for smooth movement
+                                isLaneSwitching = true
+                            }
+                        }
+                    }
+
+                    // Reset initial touch position
+                    initialTouchX = currentTouchX
                 }
-                else if (deltaX > SWIPE_THRESHOLD) {
-                    // Swiped left
-                    test = true
-                }
-                true
             }
-            else -> false
+            MotionEvent.ACTION_UP -> {
+                // Reset the lane switch state when the finger is lifted
+                isLaneSwitching = false
+            }
         }
         invalidate()
         return true
@@ -83,9 +123,9 @@ class GameView(context: Context) : View(context) {
 
     //Sets the lanes X position depending on screen size.
     fun setUpLanes(){
-        middleLaneX = getScreenWidth()/2
-        leftLaneX = getScreenWidth()/4
-        rightLaneX = getScreenWidth()/2 + getScreenWidth()/4
+        //middleLaneX = getScreenWidth()/2
+        //leftLaneX = getScreenWidth()/4
+        //rightLaneX = getScreenWidth()/2 + getScreenWidth()/4
     }
 
     fun getScreenWidth(): Int {
